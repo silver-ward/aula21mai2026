6126201c06e6df12da73e6b5e3cfaedc549167a2
silver-ward <eduard_ribeiro@hotmail.com>
2026-07-30 20:14:27 -0300

    add exercicio 6 sectioning

--- exercicios/6/biblioteca.js
+++ exercicios/6/biblioteca.js
@@ -1,34 +1,47 @@
 // #region Parte 1 – Criando o Array 
+console.log('--------------------');
+console.log('Parte 1 – Criando o Array:');
 livro = [
     'Algoritmos',
     'JavaScript',
     'Banco de Dados'];
 
 livro.push();
-
 console.log(livro);
+console.log();
 //#endregion
 
 // #region Parte 2 – Inserindo Novos Livros
+console.log('--------------------');
+console.log('Parte 2 – Inserindo Novos Livros:');
 livro.push('Redes de Computadores');
 livro.unshift('Lógica de Programação');
-
 console.log(livro);
+console.log();
 //#endregion
 
 // #region Parte 3 – Consultando Informações
+console.log('--------------------');
+console.log('Parte 3 – Consultando Informações:');
 console.log(livro[0]);
 console.log(livro[livro.length - 1]);
 console.log(livro.length);
+console.log();
 //#endregion
 
 // #region Parte 4 – Removendo Livros
+console.log('--------------------');
+console.log('Parte 4 – Removendo Livros:');
 livro.pop(livro.length - 1);
 livro.shift(livro[0]);
 console.log(livro);
+console.log();
 //#endregion
 
 // #region Parte 5 – Atualizando Informações
+console.log('--------------------');
+console.log('Parte 5 – Atualizando Informações:');
 livro[1] = 'Programação Web';
 console.log(livro);
+console.log();
 //#endregion
