8481f51bcc588d3ba317691281641ae243315c8a
silver-ward <eduard_ribeiro@hotmail.com>
2026-07-30 19:02:58 -0300

    add exercicio 6 parte 2

--- exercicios/6/biblioteca.js
+++ exercicios/6/biblioteca.js
@@ -1,7 +1,17 @@
-livro = [];
-
-livro.push('Algoritmos',
+// #region Parte 1 – Criando o Array 
+livro = [
+    'Algoritmos',
     'JavaScript',
-    'Banco de Dados');
+    'Banco de Dados'];
+
+livro.push();
 
 console.log(livro);
+//#endregion
+
+// #region Parte 2 – Inserindo Novos Livros
+livro.push('Redes de Computadores');
+livro.unshift('Lógica de Programação');
+
+console.log(livro);
+//#endregion
