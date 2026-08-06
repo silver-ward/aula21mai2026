3c62761a8155d257eab0c7c56d8f05e405ed1c82
silver-ward <eduard_ribeiro@hotmail.com>
2026-08-06 19:43:35 -0300

    add lacos.js

---
+++ lacos.js
@@ -0,0 +1,23 @@
+console.log("Laço for:");
+
+for (let i = 1; i <= 5; i++) {
+    console.log("Valor de i:", i);
+}
+
+console.log("Laço while:");
+
+let j = 1;
+
+while (j <= 5) {
+    console.log("Valor de j:", j);
+    j++;
+}
+
+console.log("Laço do...while:");
+
+let k = 5;
+
+do {
+    console.log("Valor de k:", k);
+    k--;
+} while (k >= 1);
