de831745984fc4095817a242af8cbf82e553e50e
silver-ward <eduard_ribeiro@hotmail.com>
2026-08-06 19:24:00 -0300

    add animal class

---
+++ exercicios/7/objetos/Animal.js
@@ -0,0 +1,55 @@
+const Cliente = require('./Cliente.js');
+const Prontuario = require('./Prontuario.js');
+
+class Animal{
+
+    #nome;
+    #especie;
+    #cliente;
+    #prontuario;
+
+    setNome(nome){
+        if(nome){
+            this.#nome = nome;
+        }
+        return false;
+    }
+    getNome(){
+        return this.#nome;
+    }
+
+    setEspecie(especie){
+        if (especie){
+            this.#especie = especie;
+        }
+        return false;
+    }
+
+    getEspecie(){
+        return this.#especie;
+    }
+
+    setCliente(cliente){
+        if(cliente instanceof Cliente){
+            this.#cliente = cliente;
+        }
+        return false;
+    }
+
+    getCliente(){
+        return this.#cliente;
+    }
+
+    setProntuario(prontuario){
+        if(prontuario instanceof Prontuario){
+            this.#prontuario = prontuario;
+        }
+        return false;
+    }
+
+    getProntuario(){
+        return this.#prontuario;
+    }
+}
+
+module.exports = Animal;
