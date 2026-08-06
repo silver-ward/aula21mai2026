30b611f668af4c176b47036901f5f562569098c6
silver-ward <eduard_ribeiro@hotmail.com>
2026-08-06 19:23:32 -0300

    add cliente class

---
+++ exercicios/7/objetos/Cliente.js
@@ -0,0 +1,48 @@
+const Animal = require('./Animal.js');
+
+class Cliente {
+    #nome;
+    #telefone;
+    #animais = [];
+
+    setNome(nome){
+        if (nome){
+            this.#nome = nome;
+            return true;
+        }
+        return false;
+    }
+
+    getNome(){
+        return this.#nome;
+    }
+
+    setTelefone(telefone){
+        if(telefone){
+            this.#telefone = telefone;
+            return true;
+        }
+        return false;
+    }
+
+    getTelefone(){
+        return this.#telefone;
+    }
+
+    addAnimal(animal){
+        if(animal instanceof Animal){
+            this.#animais.push(animal);
+            animal.setCliente(this);
+            return true;
+        }
+        return false;
+    }
+
+    getAnimais(){
+        return this.#animais;
+    }
+
+    listarAnimais(){
+        console.log(this.#animais);
+    }
+}
