daace96ff76502efa1c006b160854552247eb639
silver-ward <eduard_ribeiro@hotmail.com>
2026-08-27 21:06:53 -0300

    add exercicio 8 base

---
+++ exercicios/8/repeticao.js
@@ -0,0 +1,56 @@
+//#region Parte 1 - Criando o Array
+console.log('========== Parte 1 ==========');
+alunos = [
+    'Ana',
+    'Bruno',
+    'Carlos',
+    'Daniela',
+    'Eduardo',
+    'Fernanda',
+    'Gabriel'
+];
+//#endregion
+
+//#region Parte 2 - Utilizando o Laço for
+console.log('========== Parte 2 ==========');
+
+for (let i = 0; i < alunos.length; i++){
+    console.log(`Aluno ${i+1}: ${alunos[i]}`);
+    
+    if (i !== (alunos.length - 1)){
+        console.log();
+    }
+}
+//#endregion
+
+//#region Parte 3 – Utilizando o Laço while
+console.log('========== Parte 3 ==========');
+
+//#endregion
+
+//#region Parte 4 – Utilizando o Laço do...while
+console.log('========== Parte 4 ==========');
+
+//#endregion
+
+//#region Parte 5 – Utilizando o Laço for...of
+console.log('========== Parte 5 ==========');
+
+//#endregion
+
+//#region Parte 6 – Comparando os Laços
+console.log('========== Parte 6 ==========');
+
+//#endregion
+
+//#region Desafio Extra 1
+console.log('========== Desafio Extra 1 ==========');
+//#endregion
+
+//#region Desafio Extra 2
+console.log('========== Desafio Extra 2 ==========');
+//#endregion
+
+//#region Desafio Avançado
+console.log('========== Desafio Extra Avançado ==========');
+//#endregion
